5303566b97e1efc1dbf6ba2151933d9e6abb8c1a
Muktar Usman <mualiyuoox@gmail.com>
2024-06-09 13:09:00 +0100

    P

--- database/migrations/2024_04_02_150837_create_application_company_infos_table.php
+++ database/migrations/2024_04_02_150837_create_application_company_infos_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('application_company_infos', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('application_id');
-            $table->string('profile')->nullable();
-            $table->string('description_of_products')->nullable();
+            $table->longText('profile')->nullable();
+            $table->longText('description_of_products')->nullable();
             $table->longText('short_term_objectives')->nullable();
             $table->longText('medium_term_objectives')->nullable();
             $table->longText('long_term_objectives')->nullable();
